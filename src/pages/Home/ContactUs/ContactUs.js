41aeb150f1d0599da19e226e00aaf06780a013b3
mahedi <mahedianik4@gmail.com>
2022-05-11 04:11:54 +0600

    add contact from

--- src/pages/Home/ContactUs/ContactUs.js
+++ src/pages/Home/ContactUs/ContactUs.js
@@ -1,10 +1,34 @@
 import React from 'react';
-
+import { useForm } from "react-hook-form";
+import background from '../../../assets/images/appointment.png'
 const ContactUs = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (contactData) => {
+        console.log(contactData);
+    }
     return (
-        <div>
-            
+        <div className=' my-24 p-12'  style={{ background: `url(${background})` }}>
+            <div className='text-center'>
+                <h3 className='text-primary font-bold text-2xl'>Contact Us</h3>
+                <h4 className='text-white text-4xl'>Stay connected with us</h4>
+            </div>
+            <div className='lg-w-[50%] w-full mx-auto '>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className='flex flex-col'>
+                        <input className='p-2 rounded mt-4 lg:w-[450px] w-[100%] mx-auto' placeholder='Email Address' type="email" required {...register("email")} />
+                        <input className='p-2 rounded mt-4 lg:w-[450px] w-[100%] mx-auto' placeholder='Subject' type="text" {...register("subject", { required: true })} />
+                        <textarea className='p-2 h-24 rounded mt-4 lg:w-[450px] w-[100%] mx-auto' placeholder='Your message' type="text" {...register("message", { required: true })} />
+
+                        <div className='text-center mt-8'>
+                            <input className='btn  bg-gradient-to-r from-secondary to-primary text-white font-bold' type="Submit" value='submit' />
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
+
+
+
     );
 };
 
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import ContactUs from '../ContactUs/ContactUs';
 import Info from '../Info/Info';
 import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
@@ -13,6 +14,7 @@ const Home = () => {
             <Services></Services>
             <MakeAppointment></MakeAppointment>
             <Testimonial></Testimonial>
+            <ContactUs></ContactUs>
             
         </div>
     );
